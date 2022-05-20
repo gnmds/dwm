16befaf83e356957d47ac3c2025ca186661e01e1
gabel <gabriel@nagelmichaelsen.de>
2022-05-20 10:52:18 +0200

    edit color scheme

--- config.h
+++ config.h
@@ -7,15 +7,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10:antialias=true:autohint=true";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#1f1f28";
+static const char col_gray2[]       = "#2a2a37";
+static const char col_gray3[]       = "#c8c093";
+static const char col_gray4[]       = "#dcd7ba";
+static const char col_cyan[]        = "#2d4f67";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray2, col_gray4,  col_cyan  },
 };
 
 /* tagging */
